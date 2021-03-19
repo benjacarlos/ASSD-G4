a17b428ac87c21e914ca7f4ddaf7e0d6d73fc02c
nmestanza <nicolasmestanza@Nicolass-MacBook-Air.local>
2021-03-19 16:31:37 -0300

    Adding Basic PyQt App

---
+++ TP1/pythonApp/main.py
@@ -0,0 +1,42 @@
+import os
+from enum import Enum
+
+
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.Qt import pyqtSlot
+from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QFileDialog
+from src.tp1Application import myTp1Application
+
+
+if __name__ == "__main__":
+
+    app = QApplication([])
+    myMainWindow = QMainWindow()
+
+    #############################################
+    # Se aplican estilos según CSS StyleSheet   #
+    #############################################
+
+    #with open ("src/style/style.qss", "r") as f:
+    #    stylesheet = f.read()
+    #app.setStyleSheet(stylesheet)
+
+    #############################################
+
+    widget = myTp1Application()
+
+    #############################################
+    # Centralización de pantalla de aplicación  #
+    #############################################
+
+    qr = widget.frameGeometry()
+    centerPoint = QDesktopWidget().availableGeometry().center()
+    qr.moveCenter(centerPoint)
+    widget.move(qr.topLeft())
+
+    #############################################
+
+    widget.setWindowTitle("Trabajo Práctico 1 - Grupo 4 - ASSD")
+    widget.show()
+    app.exec()
---
+++ TP1/pythonApp/src/tp1Application.py
@@ -0,0 +1,18 @@
+from PyQt5.QtWidgets import *
+from src.ui.myTp1MainWindow import Ui_MainWindow
+
+
+
+class myTp1Application(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(myTp1Application, self).__init__()
+        self.setupUi(self)
+
+
+
+
+
+
+
+
+
